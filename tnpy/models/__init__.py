2a5be5f6ea1006d99e45b007956b1cfe5a4cfd74
Wei Zheng <intuitionofmind@gmail.com>
2024-09-17 18:03:31 +0800

    update

--- tnpy/models/__init__.py
+++ tnpy/models/__init__.py
@@ -1,3 +1,3 @@
-from .fermi_square import SquareTJ, SquareTJ1J2
+from .fermi_square import SquareTJ, SquareTJ1J2, SquareSigmaTJ
 from .square_xyz import SquareXYZ
 from .square_tfim import SquareTFIM
